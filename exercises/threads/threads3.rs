2adbe4e8048179aa673c5dcefb9804103389fce4
Gituser1lone <2584453432@qq.com>
2025-04-08 14:10:24 +0800

    update

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,10 +3,8 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::sync::mpsc;
-use std::sync::Arc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -26,23 +24,28 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+fn send_tx(q: Arc<Mutex<Queue>>, tx: Arc<Mutex<mpsc::Sender<u32>>>) {
+    let qc1 = Arc::clone(&q);
+    let qc2 = Arc::clone(&q);
+    let tx1 = Arc::clone(&tx);
+    let tx2 = Arc::clone(&tx);
 
+    // First thread to send first_half
     thread::spawn(move || {
-        for val in &qc1.first_half {
+        let queue = qc1.lock().unwrap();
+        for val in &queue.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.lock().unwrap().send(*val).unwrap(); // Lock tx to send
             thread::sleep(Duration::from_secs(1));
         }
     });
 
+    // Second thread to send second_half
     thread::spawn(move || {
-        for val in &qc2.second_half {
+        let queue = qc2.lock().unwrap();
+        for val in &queue.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.lock().unwrap().send(*val).unwrap(); // Lock tx to send
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -50,17 +53,19 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    let queue = Queue::new();
-    let queue_length = queue.length;
+    let tx = Arc::new(Mutex::new(tx)); // Wrap tx in Arc and Mutex
+    let queue = Arc::new(Mutex::new(Queue::new()));
+    let queue_length = queue.lock().unwrap().length;
 
     send_tx(queue, tx);
 
     let mut total_received: u32 = 0;
+    // Receive data from the channel
     for received in rx {
         println!("Got: {}", received);
         total_received += 1;
     }
 
     println!("total numbers received: {}", total_received);
-    assert_eq!(total_received, queue_length)
+    assert_eq!(total_received, queue_length);
 }
